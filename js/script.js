7e7c0e176775530bb5a495afe23f1b7e88072d2f
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-08-21 18:20:29 +0200

    add function cell

--- js/script.js
+++ js/script.js
@@ -25,30 +25,46 @@
 
 // //* Evento button per generare la griglia al click
 // button.addEventListener('click', function() {
-//     grid.innerHTML = "";
-
-//     //* Ciclo FOR
-//     for (let i = 1; i <= totalCells; i++) {
-//         const cell = generateCell();
-//         grid.appendChild(cell);
-//         cell.innerHTML = i;
-//     }
-// })
-
+    //     grid.innerHTML = "";
+    
+    //     //* Ciclo FOR
+    //     for (let i = 1; i <= totalCells; i++) {
+        //         const cell = generateCell();
+        //         grid.appendChild(cell);
+        //         cell.innerHTML = i;
+        //     }
+        // })
+        
 //* BONUS
 //* Recupero l'elemento grid
 const grid = document.getElementById('grid');
-
+        
 //* Recupero l'elemento button
 const button = document.getElementById('button');
-//* Evento button per generare la griglia al click
-button.addEventListener('click', function(){
-    generateGrid()
-})
 
 //* Recupero elemento dal DOM con id floatingSelect
 const difficultyEl = document.getElementById('floatingSelect');
-//*Funzione Griglia
+
+//* Funzione per generare una cella
+function generateCell(cellSize) {
+    for (let i = 1; i <= (cellSize * cellSize); i++) {
+        const cell = document.createElement('li');
+        cell.className = 'cell';
+        cell.innerText = i;
+        //* Controllo per le dimensioni della griglia
+        cell.style.width = (100 / cellSize) + '%';
+        cell.style.height = (100 / cellSize) + '%';
+        //* Evento sul click della cella
+        cell.addEventListener('click', function () {
+            this.classList.toggle('color_cells')
+            console.log(this.innerText)
+        });
+        
+        grid.appendChild(cell);
+    }  
+}
+
+//*Funzione per generare una griglia
 function generateGrid() {
     const difficulty = difficultyEl.value
     let size;
@@ -66,23 +82,15 @@ function generateGrid() {
     }
 
     grid.innerHTML = '';
-    //* Ciclo FOR per creazione delle celle della griglia
-    for (let i = 1; i <= (size * size); i++) {
-        const cell = document.createElement('li');
-        cell.className = 'cell';
-        cell.innerText = i;
-        //* Controllo per le dimensioni della griglia
-        cell.style.width = (100 / size) + '%';
-        cell.style.height = (100 / size) + '%';
-        //* Evento sul click della cella
-        cell.addEventListener('click', function () {
-            this.classList.toggle('color_cells')
-            console.log(this.innerText)
-        });
-
-        grid.appendChild(cell);
-    }
+    //* Invoco la funzione per creare una cella
+    generateCell(size)
 }
+
+//* Evento button per generare la griglia al click
+button.addEventListener('click', function(){
+    generateGrid()
+})
+
 
 
 
